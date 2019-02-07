2f040121488f7bd21d0b079f0697603e1d2ffeb9
Mariia Shushakova <mariia.shushakova@gmail.com>
2019-02-07 13:48:30 +0300

    lesson4

---
+++ src/main/java/base/SelenideBase.java
@@ -0,0 +1,14 @@
+package base;
+
+import com.codeborne.selenide.Browsers;
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.BeforeSuite;
+
+public class SelenideBase {
+        @BeforeSuite
+        public void beforeSuite() {
+            Configuration.browser = Browsers.CHROME;
+            Configuration.timeout = 5000;
+            Configuration.pollingInterval = 300;
+        }
+}
